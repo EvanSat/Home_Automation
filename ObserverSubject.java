12c21fc42d0ed4e921ad21b791f2a6abc216aa12
EvanSat <53355852+EvanSat@users.noreply.github.com>
2019-10-20 02:07:07 -0500

    Delete ObserverSubject.java

--- ObserverSubject.java
+++
@@ -1,8 +0,0 @@
-
-public interface ObserverSubject {
-
-	public void registerObserver(Observer newObserver);
-	public void removeObserver(Observer newObserver);
-	public void notifyObservers();
-	
-}
